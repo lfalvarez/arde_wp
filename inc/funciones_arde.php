6a7822587d57890a2d67ea639ffe6c8a5040a6c3
Felipe Álvarez <luisfelipealvarez@gmail.com>
2018-12-14 10:56:27 -0200

    Automáticamente se crea una categoríá cuando se crea una página de un minisitio

---
+++ inc/funciones_arde.php
@@ -0,0 +1,25 @@
+<?php
+// this function makes all posts in the default category private
+
+function crear_categoria_por_minisitio($post_id) {
+    $template = get_post_meta($post_id, '_wp_page_template', true);
+    $is_minisitio = $template == 'page-templates/minisitiopage.php';
+    if (!$is_minisitio){
+        return;
+    }
+
+    $title = get_the_title($post_id);
+    $slug = sanitize_title($title);
+    $existe_categoria_para_minisitio = get_category_by_slug($slug);
+    if($existe_categoria_para_minisitio){
+        return;
+    }
+    $nueva_categoria = array('cat_name' => $title,
+                             'category_description' => 'Minisitio de '. $title,
+                             'category_nicename' => $slug,
+                             'category_parent' => '');
+    wp_insert_category($nueva_categoria);
+    
+}
+add_action( 'publish_page', 'crear_categoria_por_minisitio', 10);
+?>
