eff0a018ac8553f364fcde41ad91dedae862a72b
Felipe Alvarez <luis.alvarez@thoughtworks.com>
2019-04-02 00:13:35 -0300

    Quitando los links de arriba

--- global-templates/navbar_links.php
+++ global-templates/navbar_links.php
@@ -7,10 +7,7 @@
       <li class="nav-item">
         <a class="nav-link" href="http://proyectoarde.org/data/dataset">Archivo</a>
       </li>
-      <?php $link_slug = 'colecciones'; include('link-in-navbar.php'); ?>
-
       <?php $link_slug = 'editorial'; include('link-in-navbar.php'); ?>
-      <?php $link_slug = 'sobre-archivos'; include('link-in-navbar.php'); ?>
       <?php $link_slug = 'sobre-nosotros'; include('link-in-navbar.php'); ?>
     </ul>
 
