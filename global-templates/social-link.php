83e13ae10018a1b4a48cb89b95ffe2df3ab0867d
Felipe Alvarez <luis.alvarez@thoughtworks.com>
2019-04-02 00:06:23 -0300

    arreglando los liks

--- global-templates/social-link.php
+++ global-templates/social-link.php
@@ -3,7 +3,7 @@
 $mykey_values = get_post_custom_values( $social_network , $post->ID);
 if($mykey_values){
   foreach ( $mykey_values as $key => $value ) {
-    echo '<i class="fab fa-'.$social_network.'"></i> '.$value;
+    echo '<a class="social-link" href="'.$value.'" target="_blank"><i class="fab fa-'.$social_network.'"></i> '.$value.'</a>';
   }
 }
 
