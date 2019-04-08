621df6c015b7356c6a70c77d3ad56d8908deb24d
Felipe Alvarez <luis.alvarez@thoughtworks.com>
2019-04-07 20:53:44 -0400

    Arreglando la pagina de información

--- header-pagina-informacion.php
+++ header-pagina-informacion.php
@@ -18,36 +18,38 @@ $container = get_theme_mod( 'understrap_container_type' );
 <body <?php body_class(); ?>>
 
 <div class="site" id="page">
-	<?php if (( is_front_page() && is_home() )) : ?>
-		<div class="hero-container">
-	<?php endif; ?>
+
 	<!-- ******************* The Navbar Area ******************* -->
-	<div id="wrapper-navbar" class="<?php if (( is_front_page() && is_home() )){
-		echo 'navbar-only-in-home';
-	}
-	?>" itemscope itemtype="http://schema.org/WebSite"
+	<div id="wrapper-navbar" itemscope itemtype="http://schema.org/WebSite"
 
 	>
 
 		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
 
-		<nav class="navbar navbar-expand-md navbar-dark <?php if ( is_front_page() && is_home() ) : ?>bg-arde<?php else : ?>bg-minisitio<?php endif; ?>"
+		<nav class="navbar navbar-expand-md navbar-dark bg-documento"
 			>
 
 
 
 
-		<?php if ( 'container' == $container ) : ?>
 			<div class="container">
-		<?php endif; ?>
 
-
+				<a class="navbar-brand" href="<?php echo site_url(); ?>">
+					    <img src="<?php bloginfo('template_url'); ?>/img/arde-logo-sin-frase.png" height="30" alt="arde">
+			  </a>
 
 				<?php include('global-templates/navbar_links.php') ?>
 
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
-					<span class="navbar-toggler-icon"></span>
-				</button>
+        <div class="order-3">
+		        <ul class="navbar-nav ml-auto">
+		            <li class="nav-item">
+		                <a class="nav-link" href="https://www.facebook.com/archivoarde/"><i class="fab fa-facebook-f"></i></a>
+		            </li>
+		            <li class="nav-item">
+		                <a class="nav-link" href="#"><i class="fab fa-instagram"></i></a>
+		            </li>
+		        </ul>
+		    </div>
 
 				<!-- The WordPress Menu goes here -->
 				<?php wp_nav_menu(
@@ -62,9 +64,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 						'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
 					)
 				); ?>
-			<?php if ( 'container' == $container ) : ?>
 			</div><!-- .container -->
-			<?php endif; ?>
 
 		</nav><!-- .site-navigation -->
 
--- header.php
+++ header.php
@@ -4,18 +4,22 @@ $template = get_post_meta($post->ID, '_wp_page_template', true);
 $is_minisitio = $template == 'page-templates/minisitiopage.php';
 $is_editorial = $template == 'page-templates/editorial.php';
 $is_sobre_archivos = $template == 'page-templates/sobre-archivos.php';
+$is_sobre_nosotros = $template == 'page-templates/sobre-nosotros.php';
 $is_home = is_front_page() && is_home();
 if ($is_minisitio){
 		get_header('minisitio');
 }
 elseif ($is_editorial){
-	get_header('default');
+	get_header('pagina-informacion');
 }
 elseif ($is_home){
 	get_header('default');
 }
 elseif ($is_sobre_archivos){
-	get_header('default');
+	get_header('pagina-informacion');
+}
+elseif ($is_sobre_nosotros){
+	get_header('pagina-informacion');
 }
 else {
 	get_header('documento');
