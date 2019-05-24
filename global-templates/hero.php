0145d994fb85bfd4b4ccdf7d3198a830b4d69f79
Felipe Álvarez <luisfelipealvarez@gmail.com>
2019-05-23 21:14:52 -0400

    Agregando un texto al landing

--- global-templates/hero.php
+++ global-templates/hero.php
@@ -18,6 +18,9 @@ if ( ! defined( 'ABSPATH' ) ) {
         <div class="col-8 offset-2 text-center" >
           <div class="hero_img">
             <img src="<?php bloginfo('template_url'); ?>/img/arde-logo.png" alt="">
+            <div class="text text-landing">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</div>
+            <div class="text text-landing ir-archivo-alpha">
+              <a href="http://proyectoarde.org/data/dataset" target="_blank">- Ir al archivo Alpha > </a></div>
           </div>
         </div>
 
