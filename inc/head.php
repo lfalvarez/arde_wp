826d8c75f04d0f3cd6a9ee9d3354bcfe699a4fee
Felipe Álvarez <luisfelipealvarez@gmail.com>
2019-05-23 21:21:59 -0400

    Agregando favicon

--- inc/head.php
+++ inc/head.php
@@ -6,6 +6,8 @@
 	<meta name="mobile-web-app-capable" content="yes">
 	<meta name="apple-mobile-web-app-capable" content="yes">
 	<meta name="apple-mobile-web-app-title" content="<?php bloginfo( 'name' ); ?> - <?php bloginfo( 'description' ); ?>">
+	
+	<link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/favicon.ico" />
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<style type="text/css" media="screen, print">
