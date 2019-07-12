2b2411b4f43995dd010395cad919c58f2da5f69b
jordi casanueva <jordicasanueva@gmail.com>
2019-07-11 22:47:10 -0400

    ajustes display block a social links de minisitio

--- global-templates/social-link.php
+++ global-templates/social-link.php
@@ -3,7 +3,7 @@
 $mykey_values = get_post_custom_values( $social_network , $post->ID);
 if($mykey_values){
   foreach ( $mykey_values as $key => $value ) {
-    echo '<a class="social-link" href="'.$value.'" target="_blank"><i class="fab fa-'.$social_network.'"></i> '.ucwords($social_network).'</a>';
+    echo '<a class="social-link d-block" href="'.$value.'" target="_blank"><i class="fab fa-'.$social_network.'"></i> '.ucwords($social_network).'</a>';
   }
 }
 
