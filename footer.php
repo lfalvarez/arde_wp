438f8156a2205c27721a6ddc7458d2c8c1107c6c
jordi casanueva <jordicasanueva@gmail.com>
2019-07-11 22:36:56 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -39,7 +39,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 				</div>
 
 			</div><!--col end -->
-			<div class="col-md-2 text-center text-md-left small">
+			<div class="col-md-2 gt-america text-center text-md-left small">
 
 				<a class="nav-link" href="<?php echo site_url(); ?>">Inicio</a>
 				<a class="nav-link" href="http://proyectoarde.org/data/dataset" target="_blank">Archivo</a>
