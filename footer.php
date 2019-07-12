b4cd05b2fd8f7d4a9acd2b8bf0b2e55f8db638f0
jordi casanueva <jordicasanueva@gmail.com>
2019-07-11 21:53:20 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -52,7 +52,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 			<div class="col-md-9">
 				<!-- Info Sitio-->
-				<img src="<?php bloginfo('template_url'); ?>/img/icono-footer-arde.png"/>©<?php the_date('Y'); ?>_proyecto_arde
+				<div class="text-light"><img src="<?php bloginfo('template_url'); ?>/img/icono-footer-arde.png"/>©<?php the_date('Y'); ?>_proyecto_arde</div>
 			</div><!--col end -->
 
 			<div class="col-md-3">
