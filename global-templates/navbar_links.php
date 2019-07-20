25cd0a50fc4c6e27c5d029ea75e175a9da42899f
jordi casanueva <jordicasanueva@gmail.com>
2019-07-20 12:46:03 -0400

    ajustes mobile menu

--- global-templates/navbar_links.php
+++ global-templates/navbar_links.php
@@ -1,6 +1,7 @@
+<div class="menu-mobile">
 <div class="mx-auto order-0 gt-america text-lowercase">
 
-    <ul class="navbar-nav mr-auto "  id='menu-navbar'>
+    <ul class="navbar-nav mr-auto collapse navbar-collapse"  id='menu-navbar'>
       <li class="nav-item">
         <a class="nav-link navbar-arde" href="http://proyectoarde.org/data/dataset" target="_blank">Archivo</a>
       </li>
@@ -22,3 +23,4 @@
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu-navbar">
     <span class="navbar-toggler-icon"></span>
 </button>
+</div>
