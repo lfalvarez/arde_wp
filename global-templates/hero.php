e1f918edfea5bd21f1782385f195460aebaa7334
jordi casanueva <jordicasanueva@gmail.com>
2019-07-20 11:56:30 -0400

    ajustes alto de hero image a 100vh

--- global-templates/hero.php
+++ global-templates/hero.php
@@ -27,6 +27,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <?php if (( is_front_page() && is_home() )) : ?>
    </div>
 <?php endif; ?>
+
 <script type="text/javascript">
     jQuery(function(){
         //console.log('url("<?php bloginfo('template_url'); ?>/img/fondo-arde.png")');
