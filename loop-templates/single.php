fe1a4f58fba22b98e18c68a167c8bf8f94964abe
jordi casanueva <jordicasanueva@gmail.com>
2019-07-19 22:07:11 -0400

    Update single.php

--- loop-templates/single.php
+++ loop-templates/single.php
@@ -61,6 +61,16 @@ Thumbnail en Single<div class="jumbotron jumbotron-fluid image-jumbotron">
 
 </section>
 
+<section>
+
+    <!-- Custom field: Video -->
+    <?php $embed= get_post_meta($post->ID, "Video", true); //Llamo el CF de Video ?>
+    <?php if($embed !== '') { // Si tiene "Video" mostrará algo ?>
+        <div class="videoWrapper"><?php $key="Video"; echo get_post_meta($post->ID, $key, true); ?></div>
+    <?php } else { // Si no tiene "Video" mostrará nada ?>
+
+</section>
+
 <div class="row description-doc gt-america">
 	<div class='col-md-1'>
 		<!-- Facebook -->
