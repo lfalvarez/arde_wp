6dae6380239fbd8dbb010cae4898efadb963dba2
jordi casanueva <jordicasanueva@gmail.com>
2019-07-22 18:53:47 -0400

    Update single.php

--- loop-templates/single.php
+++ loop-templates/single.php
@@ -81,7 +81,8 @@ Thumbnail en Single<div class="jumbotron jumbotron-fluid image-jumbotron">
 
              echo 'do_shortcode("';
              echo '[pdfjs-viewer url=';
-             echo $key=PDF;
+             echo $key=;
+             echo "PDF";
              echo '';
              echo get_post_meta($post->ID, $key, true);
              echo '")';
