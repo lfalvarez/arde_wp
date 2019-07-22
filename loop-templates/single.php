4a3b9849729420ca0f8cea919ffad70168bfcfc6
jordi casanueva <jordicasanueva@gmail.com>
2019-07-22 18:32:48 -0400

    ajustes pdf viewer

--- loop-templates/single.php
+++ loop-templates/single.php
@@ -74,7 +74,7 @@ Thumbnail en Single<div class="jumbotron jumbotron-fluid image-jumbotron">
     <!-- Custom field: PDF -->
     <?php $embed= get_post_meta($post->ID, "PDF", true); //Llamo el CF de PDF ?>
     <?php if($embed !== '') { // Si tiene "PDF" mostrará algo ?>
-        <div class="wrapper-PDF"><?php echo do_shortcode("[pdfjs-viewer url=<?php $key="PDF"; echo get_post_meta($post->ID, $key, true); ?> viewer_width=600px viewer_height=700px download=true]"); ?></div>
+        <div class="wrapper-PDF"><?php echo do_shortcode("[pdfjs-viewer url=<?php $key='PDF'; echo get_post_meta($post->ID, $key, true); ?> viewer_width=auto viewer_height=450px download=true]"); ?></div>
     <?php } else { // Si no tiene "PDF" mostrará nada ?>
 
     <?php } // Se cierra el condicional ?>
