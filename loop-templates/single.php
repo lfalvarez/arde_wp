f6f6caa7c483eb7f7c1a61ebd01140bd47c0df13
jordi casanueva <jordicasanueva@gmail.com>
2019-07-22 18:42:14 -0400

    ajustes shortcode pdf

--- loop-templates/single.php
+++ loop-templates/single.php
@@ -74,10 +74,30 @@ Thumbnail en Single<div class="jumbotron jumbotron-fluid image-jumbotron">
     <!-- Custom field: PDF -->
     <?php $embed= get_post_meta($post->ID, "PDF", true); //Llamo el CF de PDF ?>
     <?php if($embed !== '') { // Si tiene "PDF" mostrará algo ?>
-        <div class="wrapper-PDF"><?php echo do_shortcode("[pdfjs-viewer url=<?php $key='PDF'; echo get_post_meta($post->ID, $key, true); ?> viewer_width=auto viewer_height=450px download=true]"); ?></div>
+        <div class="wrapper-PDF"><?php echo do_shortcode("<?php  echo ; ?> viewer_width=auto viewer_height=450px download=true]"); ?></div>
+   
+
+<?php 
+
+             echo do_shortcode(;
+             echo '[pdfjs-viewer url=';
+             echo $key=;
+             echo 'PDF';
+             echo get_post_meta($post->ID, $key, true);
+
+             ?>
+
     <?php } else { // Si no tiene "PDF" mostrará nada ?>
 
     <?php } // Se cierra el condicional ?>
+
+
+
+
+
+
+
+
 
 </section>
 
