026a8f90b0cfd2546c7d71a779c13103e41552c6
jordi casanueva <jordicasanueva@gmail.com>
2019-07-23 16:16:04 -0400

    ajustes background titulo colecciones

--- page-templates/colecciones.php
+++ page-templates/colecciones.php
@@ -15,14 +15,14 @@ get_header();
 $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
-<div class="row encabezado-imagen">
-  <div class="col-12"   <?php 
+<div class="row encabezado-imagen" <?php 
 
   $featured_img_url = get_the_post_thumbnail_url($post->ID, 'large'); 
 
-  echo '<div style="background-image: url('.$featured_img_url.');"></div>'; 
+  echo 'style="background-image: url('.$featured_img_url.');"'; 
 
   ?>>
+  <div class="col-12">
       <h1 class="text-center text-uppercase titulo-encabezado-imagen">Colecciones</h1>
   </div>
 </div>
