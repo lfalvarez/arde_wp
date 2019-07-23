48b961494fa80db333e56e1672fc7c268fc92ecf
jordi casanueva <jordicasanueva@gmail.com>
2019-07-23 16:28:21 -0400

    colecciones probar sacando una parte

--- page-templates/colecciones.php
+++ page-templates/colecciones.php
@@ -29,18 +29,6 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 </div>
 
-<div class="container">
-      <div class="row lista colecciones">
-        <div class="col-2">
-          <?php while ( have_posts() ) : the_post(); ?>
-
-          	<?php get_template_part( 'loop-templates/content', 'blank' ); ?>
-
-          <?php endwhile; // end of the loop. ?>
-        </div>
-      </div>
-</div>
-
 <?php get_template_part( 'global-templates/listar-minisitios-sin-titulo' ); ?>
 
 <?php get_footer(); ?>
