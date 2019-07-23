208664fe1c58868a1c9c4899a377c1a411e3eb6a
jordi casanueva <jordicasanueva@gmail.com>
2019-07-23 16:02:36 -0400

    ajustes home colecciones

--- page-templates/colecciones.php
+++ page-templates/colecciones.php
@@ -16,9 +16,9 @@ $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
 <div class="row encabezado-imagen">
-  <?php echo '<a href="'.$featured_img_url.'" rel="lightbox">'; 
-    the_post_thumbnail('thumbnail');
-echo '</a>'; ?>
+  <?php 
+  echo '<div style="background-image:url('.$featured_img_url.')"></div>'; 
+  ?>
   <div class="col-12">
       <h1 class="text-center text-uppercase titulo">Colecciones</h1>
   </div>
