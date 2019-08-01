98b3a0d778d74da038a39743a7149aafe8118e57
jordi casanueva <jordicasanueva@gmail.com>
2019-08-01 18:34:43 -0400

    Update single.php

--- loop-templates/single.php
+++ loop-templates/single.php
@@ -144,16 +144,16 @@ Thumbnail en Single<div class="jumbotron jumbotron-fluid image-jumbotron">
 <script type="text/javascript">
 $(document).ready(function(){
   $('.single-item').slickLightbox({
-    itemSelector: 'figure > img',
-    caption: 'caption',
-    useHistoryApi: 'true',
-    lazy: true,
-    src: true
+
   });
 	var sLightbox = $(this);
 	  sLightbox.slickLightbox({
 	    src: 'src',
 	    itemSelector: 'figure > img'
+    caption: 'caption',
+    useHistoryApi: 'true',
+    lazy: true,
+    src: true
 	  });
 });
 </script>
