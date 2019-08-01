a8cd22b7298bf5dfa0341dddbbba62ce16ea9254
jordi casanueva <jordicasanueva@gmail.com>
2019-08-01 18:19:07 -0400

    Update single.php

--- loop-templates/single.php
+++ loop-templates/single.php
@@ -144,7 +144,9 @@ Thumbnail en Single<div class="jumbotron jumbotron-fluid image-jumbotron">
 <script type="text/javascript">
 $(document).ready(function(){
   $('.single-item').slickLightbox({
-    itemSelector: 'figure img'
+    itemSelector: 'figure img',
+    caption: 'caption',
+    useHistoryApi: 'true'
   });
 });
 </script>
