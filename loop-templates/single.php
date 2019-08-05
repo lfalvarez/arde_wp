056b7aeed7033a916da0711bf08a9f49855e5c96
jordi casanueva <jordicasanueva@gmail.com>
2019-08-05 14:12:08 -0400

    Update single.php

--- loop-templates/single.php
+++ loop-templates/single.php
@@ -55,7 +55,7 @@ Thumbnail en Single<div class="jumbotron jumbotron-fluid image-jumbotron">
 </div>
 <?php endif; ?> -->
 
-<section style="visibility: hidden;" class="single-item">
+<section style="visibility: hidden;" class="single-item btn btn-link">
 
     <?php the_content(); ?>
 
