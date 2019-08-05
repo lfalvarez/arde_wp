2020ede6bacfe1db044a6eef3f5b0e6da57f75c2
jordi casanueva <jordicasanueva@gmail.com>
2019-08-05 15:51:21 -0400

    Update header.php

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 $template = get_post_meta($post->ID, '_wp_page_template', true);
 $is_minisitio = $template == 'page-templates/minisitiopage.php';
 $is_editorial = $template == 'page-templates/editorial.php';
+$is_singleeditorial = $template == 'loop-templates/editorial.php';
 $is_sobre_archivos = $template == 'page-templates/sobre-archivos.php';
 $is_sobre_nosotros = $template == 'page-templates/sobre-nosotros.php';
 $is_home = is_front_page() && is_home();
@@ -10,7 +11,7 @@ if ($is_minisitio){
 		get_header('minisitio');
 }
 elseif ($is_editorial){
-	get_header('editorialarchivistica');
+	get_header('pagina-informacion');
 }
 elseif ($is_home){
 	get_header('default');
@@ -20,6 +21,9 @@ elseif ($is_sobre_archivos){
 }
 elseif ($is_sobre_nosotros){
 	get_header('pagina-informacion');
+}
+elseif ($_singleeditorial){
+	get_header('editorialarchivistica');
 }
 else {
 	get_header('documento');
