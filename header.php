28dcbe6f5dfe699e11be7ec770e32a839601ee96
jordi casanueva <jordicasanueva@gmail.com>
2019-08-06 11:35:29 -0400

    ajustes página editorial

--- header.php
+++ header.php
@@ -10,7 +10,7 @@ if ($is_minisitio){
 		get_header('minisitio');
 }
 elseif ($is_editorial){
-	get_header('editorialarchivistica');
+	get_header('pagina-informacion');
 }
 elseif ($is_home){
 	get_header('default');
