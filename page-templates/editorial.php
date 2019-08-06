f8c4d55fce9f8c4a77295c3150d79406123d1866
jordi casanueva <jordicasanueva@gmail.com>
2019-08-06 13:33:31 -0400

    Update editorial.php

--- page-templates/editorial.php
+++ page-templates/editorial.php
@@ -40,13 +40,18 @@ $query = new WP_Query( array( 'category_name' => 'editorial' ) );
 
     <?php the_title( '<h1 class="text-left">', '</h1>' ); ?>
 
-</header>
+</header> <!-- .entry-header -->
 
-</a><!-- .entry-header -->
+</a>
 
 <div class="container">
 
   <div class="row">
+
+    <div class="col-12">
+        <h2 class="text-center text-uppercase titulo">Artículos Recientes</h2>
+    </div>
+
     <div class="col-2">
 
            <?php endwhile;
