9d7dd712ed7581c6f79c2e3a93674bfa9a470e86
jordi casanueva <jordicasanueva@gmail.com>
2019-08-06 17:49:23 -0400

    Update editorial.php

--- page-templates/editorial.php
+++ page-templates/editorial.php
@@ -17,7 +17,6 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <div class="posteo">
 
-
 <?php
 
 $args = array(
@@ -40,6 +39,7 @@ if ( $query->have_posts() ) {
                 style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('<?php echo get_the_post_thumbnail_url($post->ID); ?>');
                        background-repeat: no-repeat;
                        background-size: cover;">
+</div>
 
 </article>
 
@@ -50,6 +50,7 @@ rewind_posts();
 
 ?>
 
+</div>
 
 <div class="row titulo-colecciones-home">
 
@@ -57,55 +58,6 @@ rewind_posts();
         <div class="text-center text-uppercase titulo">Artículos Recientes</div>
     </div>
 
-  </div>
-
-  <?php if (have_posts()) : ?>
-    
-    <?php 
-
-    $args = array (
-      'cat' => 5
-    );
-
-    query_posts($args) ?>
-
-        <?php while (have_posts()) : the_post(); ?>
-
-            <article>
-
-            <div
-
-            <?php if (has_post_thumbnail($post)): ?>
-                class=""
-                style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('<?php echo get_the_post_thumbnail_url($post->ID); ?>');
-                       background-repeat: no-repeat;
-                       background-size: cover;"
-            >
-
-            </div>
-
-            <a href="<?php echo get_permalink(); ?>">
-
-            <header>
-
-                <?php the_title( '<h1 class="text-left">', '</h1>' ); ?>
-
-            </header> <!-- .entry-header -->
-
-            </a>
-
-            </article>
-
-            <?php endif; ?>
-
-            <?php endwhile; ?>
-
-
-    <?php endif; ?>
-
-</div>
-
-</div>
 </div>
 
 <?php get_footer(); ?>
