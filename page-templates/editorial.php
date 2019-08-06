17bdda2204f486a323712b302f8769a41235926b
jordi casanueva <jordicasanueva@gmail.com>
2019-08-06 14:03:13 -0400

    Update editorial.php

--- page-templates/editorial.php
+++ page-templates/editorial.php
@@ -23,17 +23,12 @@ $container = get_theme_mod( 'understrap_container_type' );
 
     <div class="proyectos col2 span_1_of_4">
                     
-            <div class="proyectos-foto">
-
-            <!-- Custom field: imagen -->
-            <?php if ( has_post_thumbnail() ) : ?>
-                <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-                <li class="imagen-proyecto" style="background: url('<?php the_post_thumbnail_url(); ?>'); background-repeat: no-repeat; background-size: contain; list-style:none; background-position: center;"></li>
-                <li class="proyectos-titulo fs15 thin lh-15"><?php the_title(); ?></li>
-                </a>
-            <?php endif; ?>
-
-            </div>
+            <div <?php if (has_post_thumbnail($post)): ?>
+                      class="landing-image-editorial bg-minisitio-con-imagen"
+                      style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('<?php echo get_the_post_thumbnail_url($post->ID); ?>');
+                             background-repeat: no-repeat;
+                             background-size: cover;"
+            >
     
     </div>
 
