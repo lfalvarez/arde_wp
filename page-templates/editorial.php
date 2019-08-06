cf3b0a51a3f2b5e9f4f6ffef7a2f7e1d30786a8e
jordi casanueva <jordicasanueva@gmail.com>
2019-08-06 17:17:49 -0400

    Update editorial.php

--- page-templates/editorial.php
+++ page-templates/editorial.php
@@ -57,8 +57,9 @@ $container = get_theme_mod( 'understrap_container_type' );
 
             <?php endif; ?>
 
-            <?php endwhile; ?> 
+            <?php endwhile; ?>
 
+<?php endif; ?>
 
 <div class="row titulo-colecciones-home">
 
@@ -67,6 +68,48 @@ $container = get_theme_mod( 'understrap_container_type' );
     </div>
 
   </div>
+
+  <?php if (have_posts()) : ?>
+    
+    <?php 
+
+    $args = array (
+      'cat' => 5,
+      'offset' => 1
+    );
+
+    query_posts($args) ?>
+
+        <?php while (have_posts()) : the_post(); ?>
+
+            <article>
+
+            <div
+
+            <?php if (has_post_thumbnail($post)): ?>
+                class=""
+                style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('<?php echo get_the_post_thumbnail_url($post->ID); ?>');
+                       background-repeat: no-repeat;
+                       background-size: cover;"
+            >
+
+            </div>
+
+            <a href="<?php echo get_permalink(); ?>">
+
+            <header>
+
+                <?php the_title( '<h1 class="text-left">', '</h1>' ); ?>
+
+            </header> <!-- .entry-header -->
+
+            </a>
+
+            </article>
+
+            <?php endif; ?>
+
+            <?php endwhile; ?>
 
 
     <?php endif; ?>
