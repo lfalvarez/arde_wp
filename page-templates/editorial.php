415bb2f8a5c10e09ce6b815a5efd7106d1e576e4
jordi casanueva <jordicasanueva@gmail.com>
2019-08-06 11:49:26 -0400

    prueba quitando clses lista y colecciones

--- page-templates/editorial.php
+++ page-templates/editorial.php
@@ -22,7 +22,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <div class="container">
 
-  <div class="row lista colecciones">
+  <div class="row">
     <div class="col-10 offset-1">
 
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
@@ -30,30 +30,31 @@ $container = get_theme_mod( 'understrap_container_type' );
       endwhile;endif;?>
 
     </div>
+  </div>
 
-</div>
-      <div class="row lista colecciones">
-        <div class="col-2">
-            <?php
-            $query = new WP_Query( array( 'category_name' => 'editorial' ) );
-            ?> 
-             <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
+  <div class="row">
+    <div class="col-2">
+        <?php
+        $query = new WP_Query( array( 'category_name' => 'editorial' ) );
+        ?> 
+         <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
 
-                <div class="card" style="width: 18rem;">
-                  <a href="<?php echo get_permalink(); ?>">
-                      <img class="card-img-top" src="<?php the_post_thumbnail_url(); ?>" height= alt="<?php the_title(); ?>">
-                  </a>
-                  <div class="card-body">
-                    <a href="<?php echo get_permalink(); ?>"><p class="card-text text-center"><?php the_title(); ?></p></a>
-                  </div>
-                </div>
+            <div class="card" style="width: 18rem;">
+              <a href="<?php echo get_permalink(); ?>">
+                  <img class="card-img-top" src="<?php the_post_thumbnail_url(); ?>" height= alt="<?php the_title(); ?>">
+              </a>
+              <div class="card-body">
+                <a href="<?php echo get_permalink(); ?>"><p class="card-text text-center"><?php the_title(); ?></p></a>
+              </div>
+            </div>
 
-               <?php endwhile;
-                 wp_reset_postdata();
-                  ?>
-                 <?php endif; ?>
-        </div>
-      </div>
+           <?php endwhile;
+             wp_reset_postdata();
+              ?>
+           <?php endif; ?>
+    </div>
+  </div>
+
 </div>
 
 <?php get_footer(); ?>
