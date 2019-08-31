2809d40820621d857ea7a3aa4b964971e163c041
jordi casanueva <jordicasanueva@gmail.com>
2019-08-31 19:18:26 -0400

    Update hero.php
    Cambiando el mensaje del landing

--- global-templates/hero.php
+++ global-templates/hero.php
@@ -15,7 +15,9 @@ if ( ! defined( 'ABSPATH' ) ) {
         <div class="col-8 offset-2 text-center" >
           <div class="hero_img">
             <img src="<?php bloginfo('template_url'); ?>/img/arde-logo.png" alt="">
-            <div class="text text-landing">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</div>
+            <div class="text text-landing">
+              Somos una comunidad digital que apoya el desarrollo de archivos de procesos creativos en un marco abierto, libre y de dominio público. 
+            </div>
             <div class="text text-landing ir-archivo-alpha gt-america">
               <a href="http://archivo.proyectoarde.org/" target="_blank">- Ir a archivo Alpha > </a>
             </div>
