4572f9e6661aa2239a650a81f98f42f89839bffc
jordi casanueva <jordicasanueva@gmail.com>
2019-08-31 19:50:33 -0400

    cambios sobre nosotros

--- page-templates/sobre-nosotros.php
+++ page-templates/sobre-nosotros.php
@@ -14,14 +14,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 get_header();
 $container = get_theme_mod( 'understrap_container_type' );
 ?>
-<div class="container">
-  <div class='row '>
-      <div class="col-12">
-          <h1 class="text-center text-uppercase titulo"><?php if ( have_posts() ) : while ( have_posts() ) : the_post();
-          the_title();
-          endwhile;endif;?></h1>
-      </div>
+
+<div class="row encabezado-imagen" <?php 
+
+  $featured_img_url = get_the_post_thumbnail_url($post->ID, 'large'); 
+
+  echo 'style="background-image: url('.$featured_img_url.'); background-repeat: no-repeat; background-size: cover; background-position: center; margin: 0 auto; "'; 
+
+  ?>>
+
+  <div class="col-12">
+      <h1 class="text-center text-uppercase titulo-encabezado-imagenes text-light">the_title();</h1>
   </div>
+
+</div>
+
+<div class="container">
+
       <div class="row lista colecciones">
 
         <div class="col-10 offset-1">
