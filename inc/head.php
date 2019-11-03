90c4322ec26bb176731d252d3b98f5b9435fc044
jordi casanueva <jordicasanueva@gmail.com>
2019-11-03 13:03:49 -0300

    arreglo favicon

--- inc/head.php
+++ inc/head.php
@@ -44,27 +44,27 @@
 
     <!-- Favicon -->
 
-	<link rel="apple-touch-icon-precomposed" sizes="57x57" href="http://beta.proyectoarde.org/wp-content/themes/arde_wp/apple-touch-icon-57x57.png" />
-	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="http://beta.proyectoarde.org/wp-content/themes/arde_wp/apple-touch-icon-114x114.png" />
-	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="http://beta.proyectoarde.org/wp-content/themes/arde_wp/apple-touch-icon-72x72.png" />
-	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="http://beta.proyectoarde.org/wp-content/themes/arde_wp/apple-touch-icon-144x144.png" />
-	<link rel="apple-touch-icon-precomposed" sizes="60x60" href="http://beta.proyectoarde.org/wp-content/themes/arde_wp/apple-touch-icon-60x60.png" />
-	<link rel="apple-touch-icon-precomposed" sizes="120x120" href="http://beta.proyectoarde.org/wp-content/themes/arde_wp/apple-touch-icon-120x120.png" />
-	<link rel="apple-touch-icon-precomposed" sizes="76x76" href="http://beta.proyectoarde.org/wp-content/themes/arde_wp/apple-touch-icon-76x76.png" />
-	<link rel="apple-touch-icon-precomposed" sizes="152x152" href="http://beta.proyectoarde.org/wp-content/themes/arde_wp/apple-touch-icon-152x152.png" />
-	<link rel="icon" type="image/png" href="http://beta.proyectoarde.org/wp-content/themes/arde_wp/favicon-196x196.png" sizes="196x196" />
-	<link rel="icon" type="image/png" href="http://beta.proyectoarde.org/wp-content/themes/arde_wp/favicon-96x96.png" sizes="96x96" />
-	<link rel="icon" type="image/png" href="http://beta.proyectoarde.org/wp-content/themes/arde_wp/favicon-32x32.png" sizes="32x32" />
-	<link rel="icon" type="image/png" href="http://beta.proyectoarde.org/wp-content/themes/arde_wp/favicon-16x16.png" sizes="16x16" />
-	<link rel="icon" type="image/png" href="http://beta.proyectoarde.org/wp-content/themes/arde_wp/favicon-128.png" sizes="128x128" />
-	<link rel="shortcut icon" href="http://beta.proyectoarde.org/wp-content/themes/arde_wp/favicon.ico" />
+	<link rel="apple-touch-icon-precomposed" sizes="57x57" href="http://proyectoarde.org/wp-content/themes/arde_wp/apple-touch-icon-57x57.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="http://proyectoarde.org/wp-content/themes/arde_wp/apple-touch-icon-114x114.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="http://proyectoarde.org/wp-content/themes/arde_wp/apple-touch-icon-72x72.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="http://proyectoarde.org/wp-content/themes/arde_wp/apple-touch-icon-144x144.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="60x60" href="http://proyectoarde.org/wp-content/themes/arde_wp/apple-touch-icon-60x60.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="120x120" href="http://proyectoarde.org/wp-content/themes/arde_wp/apple-touch-icon-120x120.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="76x76" href="http://proyectoarde.org/wp-content/themes/arde_wp/apple-touch-icon-76x76.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="152x152" href="http://proyectoarde.org/wp-content/themes/arde_wp/apple-touch-icon-152x152.png" />
+	<link rel="icon" type="image/png" href="http://proyectoarde.org/wp-content/themes/arde_wp/favicon-196x196.png" sizes="196x196" />
+	<link rel="icon" type="image/png" href="http://proyectoarde.org/wp-content/themes/arde_wp/favicon-96x96.png" sizes="96x96" />
+	<link rel="icon" type="image/png" href="http://proyectoarde.org/wp-content/themes/arde_wp/favicon-32x32.png" sizes="32x32" />
+	<link rel="icon" type="image/png" href="http://proyectoarde.org/wp-content/themes/arde_wp/favicon-16x16.png" sizes="16x16" />
+	<link rel="icon" type="image/png" href="http://proyectoarde.org/wp-content/themes/arde_wp/favicon-128.png" sizes="128x128" />
+	<link rel="shortcut icon" href="http://proyectoarde.org/wp-content/themes/arde_wp/favicon.ico" />
 	<meta name="application-name" content="Proyecto Arde"/>
 	<meta name="msapplication-TileColor" content="#FFFFFF" />
-	<meta name="msapplication-TileImage" content="http://beta.proyectoarde.org/wp-content/themes/arde_wp/mstile-144x144.png" />
-	<meta name="msapplication-square70x70logo" content="http://beta.proyectoarde.org/wp-content/themes/arde_wp/mstile-70x70.png" />
-	<meta name="msapplication-square150x150logo" content="http://beta.proyectoarde.org/wp-content/themes/arde_wp/mstile-150x150.png" />
-	<meta name="msapplication-wide310x150logo" content="http://beta.proyectoarde.org/wp-content/themes/arde_wp/mstile-310x150.png" />
-	<meta name="msapplication-square310x310logo" content="http://beta.proyectoarde.org/wp-content/themes/arde_wp/mstile-310x310.png" />
+	<meta name="msapplication-TileImage" content="http://proyectoarde.org/wp-content/themes/arde_wp/mstile-144x144.png" />
+	<meta name="msapplication-square70x70logo" content="http://proyectoarde.org/wp-content/themes/arde_wp/mstile-70x70.png" />
+	<meta name="msapplication-square150x150logo" content="http://proyectoarde.org/wp-content/themes/arde_wp/mstile-150x150.png" />
+	<meta name="msapplication-wide310x150logo" content="http://proyectoarde.org/wp-content/themes/arde_wp/mstile-310x150.png" />
+	<meta name="msapplication-square310x310logo" content="http://proyectoarde.org/wp-content/themes/arde_wp/mstile-310x310.png" />
 
     <!-- Open Graph data -->
 
