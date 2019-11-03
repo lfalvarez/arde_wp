4e33f3f063a1af977ccd795eebf3d766ae583074
jordi casanueva <jordicasanueva@gmail.com>
2019-11-03 12:35:08 -0300

    Update editorial.php

--- page-templates/editorial.php
+++ page-templates/editorial.php
@@ -51,11 +51,11 @@ $container = get_theme_mod( 'understrap_container_type' );
 
                 <?php the_title( '<h1 class="text-left">', '</h1>' ); ?>
 
-                <?php the_excerpt(); ?>
+            </header> <!-- .entry-header -->
+
+            <?php the_excerpt(); ?>
 
                 <?php the_date(); ?>
-
-            </header> <!-- .entry-header -->
 
             </a>
 
