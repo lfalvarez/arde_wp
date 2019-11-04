b229de1578488442f0ed8051bba368a210e0a859
jordi casanueva <jordicasanueva@gmail.com>
2019-11-04 13:06:58 -0300

    ajustes columnas single editorial

--- loop-templates/single-editorial.php
+++ loop-templates/single-editorial.php
@@ -13,14 +13,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
 
-<div class="row gt-america">
-	<div class="col-3 small">
-		<?php $social_network = 'link'; include(TEMPLATEPATH.'/global-templates/web-link.php'); ?>
-    <?php $social_network = 'facebook'; include(TEMPLATEPATH.'/global-templates/social-link.php'); ?>
-		<?php $social_network = 'twitter'; include(TEMPLATEPATH.'/global-templates/social-link.php'); ?>
-    <?php $social_network = 'instagram'; include(TEMPLATEPATH.'/global-templates/social-link.php'); ?>
-    </div>
-    <div class="col-9 entry-content">
+<div class="row description-doc gt-america">
+	<div class='col-md-1'>
+		<!-- Facebook -->
+		<a href="https://www.facebook.com/sharer/sharer.php?u=SHAREMESSAGE" title="Share on Facebook" target="_blank" class="btn btn-facebook"><i class="fab fa-facebook-f"></i></a>
+		<!-- Google+ -->
+		<a href="https://plus.google.com/share?url=SHAREMESSAGE" title="Share on Google+" target="_blank" class="btn btn-googleplus"><i class="fab fa-google-plus-g"></i></a>
+	</div>
+    <div class="col-md-11 entry-content">
 		<?php the_content(); ?>
 
 		<?php
