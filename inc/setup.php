9da4f6e75b7bb5a1c2c01d374209ee7056f344d1
jordi casanueva <jordicasanueva@gmail.com>
2020-04-09 18:21:40 -0400

    remover [...]

--- inc/setup.php
+++ inc/setup.php
@@ -129,7 +129,7 @@ if ( ! function_exists( 'understrap_all_excerpts_get_more_link' ) ) {
 	 */
 	function understrap_all_excerpts_get_more_link( $post_excerpt ) {
 		if ( ! is_admin() ) {
-			$post_excerpt = $post_excerpt . ' [...]';
+			$post_excerpt = $post_excerpt . '';
 		}
 		return $post_excerpt;
 	}
