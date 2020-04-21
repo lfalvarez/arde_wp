b1f992dc7bc43129d5748af8a3c51d0fbb00cfa0
jordi casanueva <jordicasanueva@gmail.com>
2020-04-21 18:17:19 -0400

    ajustes listado editorial inicio

--- global-templates/listar-editorial.php
+++ global-templates/listar-editorial.php
@@ -25,15 +25,28 @@ $container = get_theme_mod( 'understrap_container_type' );
 <?php
       $query = new WP_Query
       (array(
-                 'posts_per_page'   => 5,
+                 'posts_per_page'   => 3,
              )
        );
 
        while ($query->have_posts()): $query->the_post(); ?>
-      <li>
-          <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-          <p><?php the_excerpt(); ?></p>
-     </li>
+                <div class="card col-md-4 border-0">
+
+                  <a href="<?php echo get_permalink(); ?>">
+
+                    <div class="document-image" style='background-image:url("<?php if(has_post_thumbnail()){
+                      the_post_thumbnail_url();
+                    }
+                    else {
+                      echo bloginfo('template_url').'/img/arde-logo.png';
+                    }
+                    ?>")'></div>
+                  
+                  <?php the_title( '<div class="text-left">', '</div>' ); ?> <!-- titulo -->
+                  <div style="color: #b1b1b0;" class="mt-2 text-left small-70"><?php echo the_excerpt() ?></div>
+
+                  </a>
+                </div>
 <?php endwhile;?>
 
                  <div class="card col-md-12 border-0 text-right small"><a href="https://proyectoarde.org/galerias/">Ir a todos los contenidos del blog &gt;</a></div>
