67b05b9cc55947d5874a01a4343d070907ce0785
jordi casanueva <jordicasanueva@gmail.com>
2020-04-21 18:39:03 -0400

    Update hero.php

--- global-templates/hero.php
+++ global-templates/hero.php
@@ -32,8 +32,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <script type="text/javascript">
     jQuery(function(){
-        //console.log('url("<?php bloginfo('template_url'); ?>/img/fondo-arde.png")');
-        jQuery('.hero-container').css('background-image', 'url("<?php bloginfo('template_url'); ?>/img/fondo-arde.jpg")');
+            let fondos = ["fondo1.jpg","fondo2.jpg","fondo3.jpg","fondo4.jpg", "fondo5.jpg"];
+
+    function getRandomInt(max) {
+      return Math.floor(Math.random() * Math.floor(max));
+    }
+
+    let cambioFondo = getRandomInt(fondos.length);
+    let elFondo = fondos[cambioFondo];
+    //$('.hero-container').css(`url("https://proyectoarde.org/wp-content/themes/arde_wp/img/${elFondo})`);
 
     });
 </script>
