fd18955f57d0a68d563a0e373ecbf9bfa1843e5d
jordi casanueva <jordicasanueva@gmail.com>
2020-04-21 17:48:55 -0400

    Update listar-editorial.php

--- global-templates/listar-editorial.php
+++ global-templates/listar-editorial.php
@@ -55,11 +55,7 @@ $container = get_theme_mod( 'understrap_container_type' );
                   </a>
                 </div>
 
-               <?php endwhile;
-                 wp_reset_postdata();
-                 else : ?>
-                 <p>Aún sin minisitios, puedes crear una página y decirle que el template es MiniSitio</p>
-                 <?php endif; ?>
+               <?php endwhile;?>
 
                  <div class="card col-md-12 border-0 text-right small"><a href="https://proyectoarde.org/blog/">Ir a todos los contenidos del blog &gt;</a></div>
                  
