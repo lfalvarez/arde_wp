1ab32bfe30249c0084d0543ba6bfb8d33b450d20
jordi casanueva <jordicasanueva@gmail.com>
2020-04-21 17:27:49 -0400

    Update listar-editorial.php

--- global-templates/listar-editorial.php
+++ global-templates/listar-editorial.php
@@ -26,11 +26,7 @@ $container = get_theme_mod( 'understrap_container_type' );
                 $args = array(
                     'post_type' => 'post',//it is a Page right?
                     'post_status' => 'publish',
-                    'posts_per_page' => 3,
-                    'meta_query' => array(
-                        array(
-                            'key' => '_wp_page_template',
-                        )
+                    'posts_per_page' => 3
                     )
                 );
             $query = new WP_Query($args);
