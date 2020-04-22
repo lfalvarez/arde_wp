1344030a15c4c68b81a4ec1b49a1e4f09c751030
jordi casanueva <jordicasanueva@gmail.com>
2020-04-22 07:58:19 -0400

    ajustes página listado minisitios

--- global-templates/listar-minisitios-sin-titulo.php
+++ global-templates/listar-minisitios-sin-titulo.php
@@ -12,13 +12,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
-<div class="<?php echo esc_attr( $container ); ?>">
-      <div class="row lista colecciones gt-america">
+<div class="listado-galerias <?php echo esc_attr( $container ); ?>">
+
+      <div class="row lista-colecciones gt-america">
 
             <?php
                 $args = array(
                     'post_type' => 'page',//it is a Page right?
                     'post_status' => 'publish',
+                    'posts_per_page' => 20,
                     'meta_query' => array(
                         array(
                             'key' => '_wp_page_template',
@@ -30,18 +32,21 @@ $container = get_theme_mod( 'understrap_container_type' );
             ?>
              <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
 
-                <div class="card col-md-6 border-0 p-0">
+                <div class="card col-md-4 border-0">
+
                   <a href="<?php echo get_permalink(); ?>">
-                    <p class="texto-listado-colecciones text-center text-uppercase minisitio-card-title"><?php the_title(); ?></p>
-                    <p class="boton-listado-colecciones text-center">- Ver</p>
-                    <div class="document-image-colecciones" style='background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("<?php if(has_post_thumbnail()){
+
+                    <div class="document-image" style='background-image:url("<?php if(has_post_thumbnail()){
                       the_post_thumbnail_url();
                     }
                     else {
                       echo bloginfo('template_url').'/img/arde-logo.png';
                     }
-                    ?>")'>
-                    </div>
+                    ?>")'></div>
+                  
+                  <?php the_title( '<div class="text-left">', '</div>' ); ?> <!-- titulo -->
+                  <div style="color: #b1b1b0;" class="mt-2 text-left small-70"><?php echo the_excerpt() ?></div>
+
                   </a>
                 </div>
 
@@ -49,7 +54,7 @@ $container = get_theme_mod( 'understrap_container_type' );
                  wp_reset_postdata();
                  else : ?>
                  <p>Aún sin minisitios, puedes crear una página y decirle que el template es MiniSitio</p>
-            <?php endif; ?>
-
-      </div>
+                 <?php endif; ?>
+                 
+  </div>
 </div>
