fb5e04227e0fd6cf56c43a7ffc86b1f8048abf47
jordi casanueva <jordicasanueva@gmail.com>
2020-04-29 12:58:17 -0400

    display: block a header editorial

--- page-templates/editorial.php
+++ page-templates/editorial.php
@@ -38,7 +38,8 @@ $container = get_theme_mod( 'understrap_container_type' );
                 style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('<?php echo get_the_post_thumbnail_url($post->ID); ?>');
                        background-repeat: no-repeat;
                        background-size: cover;
-                       background-position: center;"
+                       background-position: center;
+                       display:block;"
             >
 
             <header>
