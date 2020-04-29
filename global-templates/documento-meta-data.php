252e8fbd3cb79d0ed95f46d383b7f904a0c00af2
jordi casanueva <jordicasanueva@gmail.com>
2020-04-29 14:20:01 -0400

    quitar capitalize

--- global-templates/documento-meta-data.php
+++ global-templates/documento-meta-data.php
@@ -1,4 +1,4 @@
 <div class="meta-div">
   <p class="meta-titulo"><?= $title; ?></p>
-  <p class="text-capitalize"><?= get_post_meta($post->ID, $title, true); ?></p>
+  <p><?= get_post_meta($post->ID, $title, true); ?></p>
 </div>
