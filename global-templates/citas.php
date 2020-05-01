2061a3bc61a9a610e87ca17dbd66e0da6696ea76
jordi casanueva <jordicasanueva@gmail.com>
2020-05-01 15:31:45 -0400

    Update citas.php

--- global-templates/citas.php
+++ global-templates/citas.php
@@ -17,7 +17,7 @@ $container = get_theme_mod( 'understrap_container_type' );
         <div class='row'>
             <div class="col-8 mx-auto my-5">
                 <p>“Las artes y sus modos de producción, encierran todavía una bitácora enorme de historias no contadas. Dichos procesos merecen la presencia de investigadores y archiveros, que permitan registrar y preservar sus memorias. Eso es el archivo, la necesidad de detenerse a nombrar, a contar historias, es ese movimiento inversamente proporcional al silencio y al olvido.”</p>
-                <p>(A. Comedi)</p>
+                <p class="gt-america mb-0">(A. Comedi)</p>
             </div>
         </div>
 </div>
