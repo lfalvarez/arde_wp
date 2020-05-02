38a0605c72a909c8e139b1d8765edca53c524557
jordi casanueva <jordicasanueva@gmail.com>
2020-05-02 08:17:19 -0400

    ajustes layout editorial

--- loop-templates/single-editorial.php
+++ loop-templates/single-editorial.php
@@ -14,7 +14,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <div class="post-editorial row gt-america cuerpo-central">
 
-    <div class="col-md-10 entry-content offset-1">
+	<div class="col-md-3">
+
+		<p class="fecha-post pt-3"><?php echo apply_filters( 'the_date', get_the_date(), get_option( 'date_format' ), '', '' ); ?></p>
+		<p class="fecha-post pt-3">Escrito por <?php the_author(); ?></p>
+
+	</div>
+
+    <div class="col-md-9 entry-content offset-1">
 		<?php the_content(); ?>
 
 		<?php
