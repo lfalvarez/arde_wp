47ceaddf82b1de11ba7abe15e9e5655fd602cef4
jordi casanueva <jordicasanueva@gmail.com>
2020-05-02 08:55:11 -0400

    ajustes fechas / comparte en single editorial

--- loop-templates/single-editorial.php
+++ loop-templates/single-editorial.php
@@ -13,14 +13,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
-<div class="post-editorial row gt-america cuerpo-central">
+<div class="post-editorial row cuerpo-central">
 
 	<div class="col-md-2 small offset-md-1">
 
-		<p>Escrito por <?php the_author(); ?></p>
-		<p><?php echo apply_filters( 'the_date', get_the_date(), get_option( 'date_format' ), '', '' ); ?></p>
+		<p class="mb-md-0 font-weight-bold">Escrito por <?php the_author(); ?></p>
+		<p class="small"><?php echo apply_filters( 'the_date', get_the_date(), get_option( 'date_format' ), '', '' ); ?></p>
 
-        <div class="post-share">
+        <div class="post-share mt-md-4">
           Comparte
           <div class="share-links">
             <a class="popup facebook" target="_blank" href="https://www.facebook.com/sharer?u=<?php the_permalink();?>&t=<?php the_title(); ?>">facebook</a>
@@ -32,7 +32,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	</div>
 
 
-    <div class="col-md-8 entry-content">
+    <div class="col-md-8 entry-content gt-america">
 		<?php the_content(); ?>
 
 		<?php
