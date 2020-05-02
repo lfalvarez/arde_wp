6b2f26c43ff2d44163438740dbd839a6a2757389
jordi casanueva <jordicasanueva@gmail.com>
2020-05-02 08:28:01 -0400

    Update single-editorial.php

--- loop-templates/single-editorial.php
+++ loop-templates/single-editorial.php
@@ -15,10 +15,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <div class="post-editorial row gt-america cuerpo-central">
 
-	<div class="col-md-3">
+	<div class="col-md-3 small">
 
-		<p class="fecha-post pt-3"><?php echo apply_filters( 'the_date', get_the_date(), get_option( 'date_format' ), '', '' ); ?></p>
-		<p class="fecha-post pt-3">Escrito por <?php the_author(); ?></p>
+		<p><?php echo apply_filters( 'the_date', get_the_date(), get_option( 'date_format' ), '', '' ); ?></p>
+		<p>Escrito por <?php the_author(); ?></p>
 
 	</div>
 
