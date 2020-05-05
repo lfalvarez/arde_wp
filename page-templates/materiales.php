99da27092c290bda40d2354c345ad88a1df6124b
jordi casanueva <jordicasanueva@gmail.com>
2020-05-05 17:17:35 -0400

    header materiales

--- page-templates/materiales.php
+++ page-templates/materiales.php
@@ -14,48 +14,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 get_header();
 $container = get_theme_mod( 'understrap_container_type' );
 ?>
-<div class="container">
-  <div class='row '>
-      <div class="col-12">
-          <h1 class="text-center text-uppercase titulo">Materiales</h1>
+
+<div class="row encabezado-imagen" <?php 
+
+  $featured_img_url = get_the_post_thumbnail_url($post->ID, 'large'); 
+
+  echo 'style="background-image: url('.$featured_img_url.'); background-repeat: no-repeat; background-size: cover; background-position: center; margin: 0 auto; "'; 
+
+  ?>>
+
+  <div class="col-12">
+    <div style="margin: 70px 0;" class="titulo-encabezado-imagenes">
+        <h1 class="text-center text-uppercase titulo-encabezado-imagenes mb-3 text-light"><?php the_title() ?></h1>
+        <div class="bajada-titulo-encabezado-imagenes text-center text-light mx-auto gt-america"><div><?php echo the_excerpt() ?></div></div>
       </div>
-  </div>
-  <div class="row lista colecciones">
-    <div class="col-2">
-
-
-      <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
-      the_content();
-      endwhile;endif;?>
-
-    </div>
-
-</div>
-      <div class="row lista colecciones">
-        <div class="col-2">
-
-            <?php
-            $query = new WP_Query( array( 'category_name' => 'materiales' ) );
-            ?>
-             <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
-
-                <div class="card" style="width: 18rem;">
-                  <a href="<?php echo get_permalink(); ?>">
-                      <img class="card-img-top" src="<?php the_post_thumbnail_url(); ?>" height= alt="<?php the_title(); ?>">
-                  </a>
-                  <div class="card-body">
-                    <a href="<?php echo get_permalink(); ?>"><p class="card-text text-center"><?php the_title(); ?></p></a>
-                  </div>
-                </div>
-
-               <?php endwhile;
-                 wp_reset_postdata();
-                  ?>
-                 <?php endif; ?>
-
-        </div>
 
   </div>
+
 </div>
 
 <?php get_footer(); ?>
