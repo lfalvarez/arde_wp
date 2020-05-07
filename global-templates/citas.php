f54df1ccb52b747fd4f6b6d324c4cb00917b1d33
jordi casanueva <jordicasanueva@gmail.com>
2020-05-07 17:26:11 -0400

    container-fluid citas

--- global-templates/citas.php
+++ global-templates/citas.php
@@ -13,7 +13,7 @@ get_header();
 $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
-<div class="<?php echo esc_attr( $container ); ?> naranjo-claro-arde-bg naranjo-arde">
+<div class="container-fluid naranjo-claro-arde-bg naranjo-arde">
         <div class='row'>
             <div class="col-8 mx-auto my-5">
                 <p>“Las artes y sus modos de producción, encierran todavía una bitácora enorme de historias no contadas. Dichos procesos merecen la presencia de investigadores y archiveros, que permitan registrar y preservar sus memorias. Eso es el archivo, la necesidad de detenerse a nombrar, a contar historias, es ese movimiento inversamente proporcional al silencio y al olvido.”</p>
