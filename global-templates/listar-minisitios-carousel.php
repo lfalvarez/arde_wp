6a667232fd45888509139e772e7fcf6fe65b3056
jordi casanueva <jordicasanueva@gmail.com>
2020-05-08 10:31:55 -0400

    Update listar-minisitios-carousel.php

--- global-templates/listar-minisitios-carousel.php
+++ global-templates/listar-minisitios-carousel.php
@@ -26,7 +26,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
                   <?php
                       $args = array(
-                          'name' => 'colectivo-zoologico, catalina-devia, teatro-nino-proletario',
+                          'name' => 'colectivo-zoologico',
                           'posts_per_page' => 10,
                           'meta_query' => array(
                               array(
