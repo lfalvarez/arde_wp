7d4e78aea37606564c066c2eafba53d597401bc5
jordi casanueva <jordicasanueva@gmail.com>
2020-05-07 23:18:02 -0400

    Update cita-2.php

--- global-templates/cita-2.php
+++ global-templates/cita-2.php
@@ -17,7 +17,6 @@ $container = get_theme_mod( 'understrap_container_type' );
         <div class='row'>
             <div class="col-8 mx-auto my-5">
                 <p>"La imagen arde en la destrucción, en el fuego que casi la carboniza, del cual sin embargo emergió y al cual ahora puede hacer imaginable. Arde en el fulgor, es decir, en la posibilidad visual que se abrió a partir de su misma extinción. Finalmente, la imagen arde de memoria, es decir, flamea aún incluso cuando ya es ceniza".</p>
-                <p class="gt-america mb-0">(A. Comedi)</p>
             </div>
         </div>
 </div>
