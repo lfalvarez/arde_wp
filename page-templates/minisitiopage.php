e27a5e8455f4fe2f9fe08a718e7880c6ee19ff1c
jordi casanueva <jordicasanueva@gmail.com>
2020-05-08 13:24:37 -0400

    Update minisitiopage.php

--- page-templates/minisitiopage.php
+++ page-templates/minisitiopage.php
@@ -25,10 +25,10 @@ $container = get_theme_mod( 'understrap_container_type' );
 
                 <main class="site-main" id="main" role="main">
 
-
+                    <?php while ( have_posts() ) : the_post(); ?>
                         <?php get_template_part( 'loop-templates/content', 'minisitio' ); ?>
 
-
+                    <?php endwhile; // end of the loop. ?>
 
                 </main><!-- #main -->
 
