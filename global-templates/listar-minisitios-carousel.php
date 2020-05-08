e27d128cbe8cdcffeef4fe3fe5f8e9b47b2ffb27
jordi casanueva <jordicasanueva@gmail.com>
2020-05-08 12:53:34 -0400

    Update listar-minisitios-carousel.php

--- global-templates/listar-minisitios-carousel.php
+++ global-templates/listar-minisitios-carousel.php
@@ -62,7 +62,6 @@ $container = get_theme_mod( 'understrap_container_type' );
                        <?php endif; ?>                  
 
         </div>
-
         <div class="ir-a-seccion col-md-12 border-0 text-right small gt-america"><a href="https://proyectoarde.org/galerias/">Ir a todas las galerías &gt;</a></div>
 
 </div>
