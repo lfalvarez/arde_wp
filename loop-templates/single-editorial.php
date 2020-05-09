6db7d10f952ecd3b5094b17047be562394a64a78
jordi casanueva <jordicasanueva@gmail.com>
2020-05-09 18:20:03 -0400

    ajustes varios y márgenes editorial

--- loop-templates/single-editorial.php
+++ loop-templates/single-editorial.php
@@ -16,22 +16,29 @@ if ( ! defined( 'ABSPATH' ) ) {
 <div class="post-editorial row cuerpo-central">
 
 	<div class="col-md-2 small offset-md-1">
+		<div class="row">
 
-		<p class="mb-md-0 font-weight-bold">Escrito por <?php the_author(); ?></p>
-		<p class="small"><?php echo apply_filters( 'the_date', get_the_date(), get_option( 'date_format' ), '', '' ); ?></p>
+			<div class="col-6 col-md-12">
 
-        <div class="post-share mt-md-4">
-          Comparte
-          <div class="share-links">
-            <a class="popup facebook" target="_blank" href="https://www.facebook.com/sharer?u=<?php the_permalink();?>&t=<?php the_title(); ?>">facebook</a>
-            <a class="popup twitter"  target="_blank" href="http://twitter.com/intent/tweet?text=Currently reading <?php the_title(); ?>&amp;url=<?php the_permalink(); ?>" title="Click to share this post on Twitter">twitter</a>
-          </div>
-        </div>
+				<p class="mb-0 font-weight-bold">Escrito por <?php the_author(); ?></p>
+				<p class="small"><?php echo apply_filters( 'the_date', get_the_date(), get_option( 'date_format' ), '', '' ); ?></p>
+
+			</div>
+
+	        <div class="col-6 col-md-12 post-share mt-md-4">
+	          <div class="share-links float-right float-md-left">
+	          	<p>Comparte</p>
+	            <a class="popup facebook" target="_blank" href="https://www.facebook.com/sharer?u=<?php the_permalink();?>&t=<?php the_title(); ?>">facebook</a>
+	            <a class="popup twitter"  target="_blank" href="http://twitter.com/intent/tweet?text=Currently reading <?php the_title(); ?>&amp;url=<?php the_permalink(); ?>" title="Click to share this post on Twitter">twitter</a>
+	          </div>
+	        </div>
+
+    	</div>
 
 	</div>
 
 
-    <div class="col-md-8 entry-content gt-america">
+    <div class="col-md-8 entry-content gt-america pt-3 pt-md-0">
 		<?php the_content(); ?>
 
 		<?php
