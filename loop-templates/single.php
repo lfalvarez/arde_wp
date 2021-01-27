6794708a4d408c10a04c4f8fe0505943005d1908
jordi casanueva <jordicasanueva@gmail.com>
2021-01-27 12:55:44 -0300

    Update single.php

--- loop-templates/single.php
+++ loop-templates/single.php
@@ -91,7 +91,7 @@ Thumbnail en Single<div class="jumbotron jumbotron-fluid image-jumbotron">
 	    <!-- Custom field: Autor -->
 	    <?php $embed= get_post_meta($post->ID, 'Autor', true); //Llamo el CF de Autor ?>
 	    <?php if($embed !== '') { // Si tiene "Autor" mostrará algo ?>
-	        <?php $key='Autor'; echo get_post_meta($post->ID, $key, true); ?>" target="_blank">
+	        <?php $key='Autor'; echo get_post_meta($post->ID, $key, true); ?>
 	    <?php } else { // Si no tiene "Autor" mostrará nada ?>
 
 	    <?php } // Se cierra el condicional ?>
@@ -99,7 +99,7 @@ Thumbnail en Single<div class="jumbotron jumbotron-fluid image-jumbotron">
 	    <!-- Custom field: Autora -->
 	    <?php $embed= get_post_meta($post->ID, 'Autora', true); //Llamo el CF de Autora ?>
 	    <?php if($embed !== '') { // Si tiene "Autora" mostrará algo ?>
-	        <?php $key='Autora'; echo get_post_meta($post->ID, $key, true); ?>" target="_blank">
+	        <?php $key='Autora'; echo get_post_meta($post->ID, $key, true); ?>
 	    <?php } else { // Si no tiene "Autora" mostrará nada ?>
 
 	    <?php } // Se cierra el condicional ?>
@@ -107,7 +107,7 @@ Thumbnail en Single<div class="jumbotron jumbotron-fluid image-jumbotron">
 	    <!-- Custom field: Autores -->
 	    <?php $embed= get_post_meta($post->ID, 'Autores', true); //Llamo el CF de Autores ?>
 	    <?php if($embed !== '') { // Si tiene "Autores" mostrará algo ?>
-	        <?php $key='Autores'; echo get_post_meta($post->ID, $key, true); ?>" target="_blank">
+	        <?php $key='Autores'; echo get_post_meta($post->ID, $key, true); ?>
 	    <?php } else { // Si no tiene "Autores" mostrará nada ?>
 
 	    <?php } // Se cierra el condicional ?>
@@ -115,7 +115,7 @@ Thumbnail en Single<div class="jumbotron jumbotron-fluid image-jumbotron">
 	    <!-- Custom field: Autoras -->
 	    <?php $embed= get_post_meta($post->ID, 'Autoras', true); //Llamo el CF de Autoras ?>
 	    <?php if($embed !== '') { // Si tiene "Autoras" mostrará algo ?>
-	        <?php $key='Autoras'; echo get_post_meta($post->ID, $key, true); ?>" target="_blank">
+	        <?php $key='Autoras'; echo get_post_meta($post->ID, $key, true); ?>
 	    <?php } else { // Si no tiene "Autoras" mostrará nada ?>
 
 	    <?php } // Se cierra el condicional ?>
@@ -123,7 +123,7 @@ Thumbnail en Single<div class="jumbotron jumbotron-fluid image-jumbotron">
 	    <!-- Custom field: Lugar -->
 	    <?php $embed= get_post_meta($post->ID, "Lugar", true); //Llamo el CF de Lugar ?>
 	    <?php if($embed !== '') { // Si tiene "Lugar" mostrará algo ?>
-	        <?php $key="Lugar"; echo get_post_meta($post->ID, $key, true); ?>" target="_blank">
+	        <?php $key="Lugar"; echo get_post_meta($post->ID, $key, true); ?>
 	    <?php } else { // Si no tiene "Lugar" mostrará nada ?>
 
 	    <?php } // Se cierra el condicional ?>
@@ -133,7 +133,7 @@ Thumbnail en Single<div class="jumbotron jumbotron-fluid image-jumbotron">
 	    <!-- Custom field: Descarga -->
 	    <?php $embed= get_post_meta($post->ID, "Descarga", true); //Llamo el Descarga ?>
 	    <?php if($embed !== '') { // Si tiene "Descarga" mostrará algo ?>
-	        <?php $key="Descarga"; echo get_post_meta($post->ID, $key, true); ?>" target="_blank">
+	        <?php $key="Descarga"; echo get_post_meta($post->ID, $key, true); ?>
 	    <?php } else { // Si no tiene "Descarga" mostrará nada ?>
 
 	    <?php } // Se cierra el condicional ?>
